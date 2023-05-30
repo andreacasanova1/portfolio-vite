6ffca75c09a81316b8a6e4023841811db9db3aa4
andreacasanova1 <116755988+andreacasanova1@users.noreply.github.com>
2023-05-30 18:21:23 +0200

    tratando de enrutar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import casanova from '../public/casanova.svg'
-import Portfolio from './Porfolio'
+
 
 
 
--- src/Porfolio.jsx
+++ src/Porfolio.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react'
 import './App.css'
 import casanova from '../public/casanova.svg'
